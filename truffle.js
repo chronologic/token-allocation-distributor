7a4970736d9006a22a7d4ff89734f5b4f9fe02d9
Anthony <adibas03@ymail.com>
2018-09-19 21:24:55 +0100

    16

--- truffle.js
+++ truffle.js
@@ -1,6 +1,6 @@
-// const HDWalletProvider = require("truffle-hdwallet-provider");
+const HDWalletProvider = require("truffle-hdwallet-provider");
 
-// const mnemonic = "rookie hungry unhappy leave rich town december option note hollow seven draft";
+const mnemonic = "rookie hungry unhappy leave rich town december option note hollow seven draft";
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -16,13 +16,13 @@ module.exports = {
     kovan: {
       gas: 4700000,
       gasPrice: 10000000000, // 10 gwei
-      // provider: new HDWalletProvider(mnemonic, "https://kovan.infura.io"),
+      provider: new HDWalletProvider(mnemonic, "https://kovan.infura.io"),
       network_id: "42"
     },
     ropsten: {
       gas: 4700000,
       gasPrice: 10000000000, // 10 gwei
-      // provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/06af9f3e62cf41d3a458c346b7dca44c"),
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/06af9f3e62cf41d3a458c346b7dca44c"),
       network_id: "3"
     }
   },
